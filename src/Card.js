97a121b64bd711d809049a6b7cbbd945ff1f0e50
sharath.hanswadi <shanswadi@gmail.com>
2019-03-08 00:40:22 +0530

    first commit

---
+++ src/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const card = (props) =>{
+
+    return (
+
+        <div className="bg-light-green dib br3 pd3 ma3 grow bw2 shadow-5"
+        > 
+        <img alt='robots' src ='https://robohash.org/sharath? 200 * 200 '  />        
+        <div>
+            <h2>{props.name} </h2>
+            <p>{props.email}</p>
+        </div>
+        </div>
+    );
+}
+
+export default card;
